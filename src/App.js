b82107d0cec401f716366b0ff54580d1e3eafef4
zubairafzal100 <zubairafzal100@gmail.com>
2023-01-06 11:24:16 +0400

    updated food.js file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Food from './Food';
 function App() {
   return (
     <div className='App'>
-      <Food dish="1" dishName="Biryani" sweetDish="Bangali Rus Gulle"/>
+      <Food dish="1" dishName="Chicken Biryani" sweetDish="Bangali Rus Gulle"/>
       <br />
       <Food dish="2" dishName="Chicken Korma" sweetDish="Gajar Ka Halwa"/>
       <br />
