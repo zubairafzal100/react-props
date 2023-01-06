d7c84ed0a7b8f09b0b7124cf22aec2b86a50c20f
zubairafzal100 <zubairafzal100@gmail.com>
2023-01-06 11:29:07 +0400

    updated food.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <Food dish="1" dishName="Chicken Biryani" sweetDish="Bangali Rus Gulle"/>
       <br />
-      <Food dish="2" dishName="Chicken Korma" sweetDish="Gajar Ka Halwa"/>
+      <Food dish="2" dishName="Beef Korma" sweetDish="Gajar Ka Halwa"/>
       <br />
       <Food dish="3" dishName="Motton Karahi" sweetDish="Trifle"/>
     </div>
